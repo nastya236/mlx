03b7eeb53287671680be8415c036850ff3589896
Anastasiia Filippova <anastasiia.filippovaa@gmail.com>
2025-08-07 02:25:05 +0200

    _

--- python/tests/nccl_test_distributed.py
+++ python/tests/nccl_test_distributed.py
@@ -13,7 +13,8 @@ class TestNCCLDistributed(mlx_tests.MLXTestCase):
         world = mx.distributed.init(strict=True, backend="nccl")
         rank = world.rank()
         mx.set_default_device(mx.Device(mx.gpu, rank % 8))
-        print(f"Rank {rank} set to device {mx.get_default_device()}")
+        print(f"Rank {rank}")
+        
     def test_all_reduce(self):
         world = mx.distributed.init()
         dtypes = [
