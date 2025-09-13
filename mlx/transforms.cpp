8eeae6bf75f173128fe9ab4f024820cb67e83b67
Anastasiia Filippova <anastasiia.filippovaa@gmail.com>
2025-09-13 17:56:54 +0200

    _

--- mlx/transforms.cpp
+++ mlx/transforms.cpp
@@ -234,8 +234,7 @@ array eval_impl(std::vector<array> outputs, bool async) {
     }
 
     if (scheduler::n_active_tasks() > MAX_ACTIVE_TASKS ||
-        (get_active_memory() > get_memory_limit() &&
-         scheduler::n_active_tasks() > 0)) {
+        (get_active_memory() > get_memory_limit())) {
       // Commit any open streams
       for (auto i : open_streams) {
         auto s = get_stream(i);
