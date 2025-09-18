d517955ea7c0da58e06f734f76ebce804c4c8b36
Anastasiia Filippova <anastasiia.filippovaa@gmail.com>
2025-09-19 00:32:09 +0200

    iostream

--- mlx/backend/cuda/allocator.cpp
+++ mlx/backend/cuda/allocator.cpp
@@ -3,7 +3,7 @@
 #include "mlx/backend/cuda/allocator.h"
 #include "mlx/backend/cuda/utils.h"
 #include "mlx/utils.h"
-
+#include <iostream>
 #include <cuda_runtime.h>
 #include <fmt/format.h>
 #include <unistd.h>
